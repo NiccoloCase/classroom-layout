ed83db2926ba825a2c3f59e3986d77ecf96f3a61
NiccoloCase <41541220+NiccoloCase@users.noreply.github.com>
2020-03-27 23:36:49 +0100

    add apollo to client

---
+++ packages/web/src/graphql/index.ts
@@ -0,0 +1,16 @@
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloLink } from "apollo-link";
+import config from "config";
+
+const cache = new InMemoryCache();
+const httpLink = new HttpLink({
+    uri: `${config.server.DOMAIN}/grapqhl`
+});
+
+export const client = new ApolloClient({
+    connectToDevTools: !config.isProduction,
+    link: ApolloLink.from([httpLink]),
+    cache
+});
