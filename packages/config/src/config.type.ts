14cd4664afbcf28c3c8584fe4866c7ad9730de75
NiccoloCase <41541220+NiccoloCase@users.noreply.github.com>
2020-04-18 22:51:11 +0200

    removed the domain as a configuration property

--- packages/config/src/config.type.ts
+++ packages/config/src/config.type.ts
@@ -6,8 +6,6 @@ export interface IConfiguration {
     server: {
         /** Porta del server */
         PORT: number;
-        /** Dominio del server */
-        DOMAIN: string;
     },
     database: {
         /** URI di conessione al server */
--- packages/config/src/config_development.ts
+++ packages/config/src/config_development.ts
@@ -4,8 +4,7 @@ export const developmentKeys: IConfiguration = {
     APP_NAME: "Classroom layout",
     isProduction: false,
     server: {
-        PORT: 5000,
-        DOMAIN: "http://localhost:5000"
+        PORT: 5000
     },
     database: {
         URI: "mongodb://admin:AdminDatabas3@ds029585.mlab.com:29585/classroom_layout-dev"
