8f68ffd61a0b81c802d703f84ed41cf46638787a
NiccoloCase <41541220+NiccoloCase@users.noreply.github.com>
2020-04-18 22:51:11 +0200

    removed the domain as a configuration property

--- packages/config/src/config_production.ts
+++ packages/config/src/config_production.ts
@@ -5,7 +5,6 @@ export const productionKeys: IConfiguration = {
     isProduction: true,
     server: {
         PORT: Number(process.env.PORT!),
-        DOMAIN: process.env.DOMAIN!
     },
     database: {
         URI: process.env.MONGO_URI!
