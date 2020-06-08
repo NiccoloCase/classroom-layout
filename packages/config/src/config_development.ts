901fe4b699583966a6ab138b1ee6c677259d7aaf
NiccoloCase <41541220+NiccoloCase@users.noreply.github.com>
2020-06-08 08:36:00 +0200

    deply test 2

---
+++ packages/config/src/config_development.ts
@@ -0,0 +1,18 @@
+import { IConfiguration } from "./config.type";
+
+export const developmentKeys: IConfiguration = {
+    APP_NAME: "Classroom layout",
+    isProduction: false,
+    server: {
+        PORT: 5000,
+        WEB_APP_DOMAIN: "localhost:3000"
+    },
+    database: {
+        URI: "mongodb://admin:AdminDatabas3@ds029585.mlab.com:29585/classroom_layout-dev"
+    },
+    emailService: {
+        SENDER: "caselli.niccolo.developer@gmail.com",
+        USER: "99a61f513d831fec7bfba6640a0c12a8",
+        PASSWORD: "ad581a019ce95184be38f92de87e359d"
+    }
+}
