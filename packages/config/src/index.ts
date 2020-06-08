11c5663402f130aa09ec8349475ef35e5fb7c3f4
NiccoloCase <41541220+NiccoloCase@users.noreply.github.com>
2020-06-08 10:12:28 +0200

    deploy test 3

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -8,7 +8,14 @@ const config: IConfiguration = (function (env) {
         case 'test':
             return require("./config_development").developmentKeys;
         default:
-            return require("./config_development").developmentKeys;
+            try {
+                // controlla che il modulo esista 
+                const dev = require("./config_development");
+                return dev.developmentKeys;
+            }
+            catch (e) {
+                console.error("it is not possible to load the configuration module for development");
+            }
     }
 })(process.env.NODE_ENV);
 
